f2e0bd6de6bad8b2e615ffbe6f86f70ee7b50709
marvin espira <marvinespira@adalabsafrica.com>
2022-06-20 17:23:05 +0300

    feat: header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,31 @@
-import { SearchIcon } from '@heroicons/react/solid'
+import { useState, useEffect, useRef } from 'react'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
 function Header() {
+    const [isScrolled, setIsScrolled ] = useState(false)
+    const isMounted = useRef(false)
+
+    useEffect(() => {
+        if (isMounted.current) return
+          isMounted.current = true
+        console.log('header mounted')
+
+        const handleScroll = () => {
+         if (window.scrollY > 0) {
+            setIsScrolled(true)        
+        console.log('setIsScrolled TRUE')
+         } else {
+            setIsScrolled(false)
+         }
+
+         window.addEventListener('scroll', handleScroll)
+
+         return () => window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+    
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-red-500'}`}>
         {/* left header section */}
         <div className="flex items-center space-x-2 md:space-x-10 ">
             <img
@@ -19,11 +43,23 @@ function Header() {
                 <li className="headerLink">New & Popular</li>
                 <li className="headerLink">My List</li>
               </ul>
+
     
         </div>
+        isScrolled { isScrolled}
+
         {/* right header section */}
-        <div>
-            <SearchIcon />
+        <div className="flex items-center space-x-4 text-sm font-light">
+            <SearchIcon className='hidden w-6 h-6 sm:inline'/>
+            <p  className='hidden lg:inline'>Kids</p>
+            <BellIcon className='w-6 h-6' />
+            <Link href="/account">
+            <img
+                src="https://rb.gy/g1pwyx"
+                alt=""
+                className="rounded cursor-pointer"
+          />
+            </Link>
         </div>
     </header>
   )
