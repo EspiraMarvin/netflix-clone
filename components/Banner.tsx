6c9dcd9edf7957128164130e30ade4de5fee45db
marvin espira <marvinespira@adalabsafrica.com>
2022-06-20 19:14:55 +0300

    feat: updates

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+function Banner() {
+  return (
+    <div className="">
+        {/* <div>
+            <Image />
+        </div> */}
+    </div>
+  )
+}
+
+export default Banner
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
       <Header />
 
       <main>
-        {/* { Banner} */}
+         <Banner />
         <section>
           {/* { Row } */}
           {/* { Row } */}
