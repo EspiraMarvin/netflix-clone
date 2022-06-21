3e8b7cd7d890f16c6eb95994946f0a128304ce51
marvin espira <marvinespira@adalabsafrica.com>
2022-06-21 20:15:44 +0300

    feat: updates

--- components/Row.tsx
+++ components/Row.tsx
@@ -14,28 +14,42 @@ interface Props {
 
   
 function Row({ title, movies}: Props) {
-    // const rowRef = useRef<HTMLDivElement>(null)
-    const rowRef = useRef(null)
-
+    const rowRef = useRef<HTMLDivElement>(null)
     const [isMoved, setIsMoved] = useState(false)
 
     const handleClick = (direction: string) => {
-        console.log('direction', direction)
+        setIsMoved(true)
+        if (rowRef.current) {
+            // console.log('rowRef current', rowRef.current)
+            const { scrollLeft, clientWidth } = rowRef.current
+            console.log('scrollLeft ', scrollLeft)
+            console.log('clientWidth ', clientWidth)
+
+            const scrollTo = 
+              direction === "left"
+               ? scrollLeft - clientWidth
+               : scrollLeft + clientWidth
+            // console.log('scrollTo ', scrollTo)
+
+            
+            rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+        }
     }
 
+    // console.log(rowRef.current!.scrollLeft, rowRef.current!.clientWidth)
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
         <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
         {title}
         </h2>
         <div ref={rowRef} className="relative group md:-ml-2">
-            <ChevronLeftIcon className="chevronDirectionIcon" onClick={() => handleClick('left')} />
+            <ChevronLeftIcon className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${!isMoved && 'hidden'}`} onClick={() => handleClick('left')} />
             <div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
                 { movies.map(movie => (
                     <ThumbNail key={movie.id} movie={movie} />
                 ))}
             </div>
-            <ChevronRightIcon className='chevronDirectionIcon' onClick={() => handleClick('right')}  />
+            <ChevronRightIcon className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`} onClick={() => handleClick('right')}  />
         </div>
     </div>
   )
