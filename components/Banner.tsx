ea28ebf9dbd2e9f51f737168c7ce91a2971c6acd
marvin espira <marvinespira@adalabsafrica.com>
2022-06-21 12:12:40 +0300

    feat: banner done

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,43 @@
 import Image from 'next/image'
+import { Movie } from '../typings'
+import { useEffect, useState } from 'react'
+import { baseUrl } from '../utils/imageUrl'
+import  { FaPlay} from "react-icons/fa"
+import { InformationCircleIcon } from '@heroicons/react/solid'
 
-function Banner() {
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null)
+useEffect(() => {
+  // select a random movie and display to the banner
+  setMovie( netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)] )
+}, [netflixOriginals])
+
+console.log('random movie', movie)
+
+
   return (
-    <div className="">
-        {/* <div>
-            <Image />
-        </div> */}
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12 xl:pb-7 2xl:pb-4">
+         <div className='absolute top-0 left-0 -z-10 h-[95vh] w-screen'>
+            <Image
+             src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+             layout='fill'
+             objectFit='cover'
+            />
+         </div> 
+
+         <h1 className='text-2xl font-bold md:text-4xl lg:text-5xl xl:text-7xl 2xl:text-9xl'>{movie?.title || movie?.original_name}</h1>
+         <p className='max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl text-shadow-md'>
+           {movie?.overview}
+          </p>
+
+         <div className='flex space-x-3'>
+          <button className="text-black bg-white bannerButton"><FaPlay className="w-4 h-4 text-black md:h-7 md:w-7" /> Play</button>
+          <button className="bannerButton bg-[gray]/50"> <InformationCircleIcon className='w-5 h-5 md:h-8 md:w-8' />  More Info</button>
+         </div>
     </div>
   )
 }
