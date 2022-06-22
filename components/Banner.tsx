c86a3c8764c06fb3fc3e6e5421a7666efe87ec41
marvin espira <marvinespira@adalabsafrica.com>
2022-06-22 12:41:16 +0300

    updates

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -16,11 +16,9 @@ useEffect(() => {
   setMovie( netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)] )
 }, [netflixOriginals])
 
-// console.log('random movie', movie)
-
   return (
     // <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-      <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12 xl:pb-7 2xl:pb-4">  
+      <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12 xl:pb-7 2xl:pb-4">
 
          <div className='absolute top-0 left-0 -z-10 h-[95vh] w-screen'>
             <Image
@@ -30,8 +28,8 @@ useEffect(() => {
             />
          </div> 
 
-         <h1 className='text-2xl font-bold md:text-4xl lg:text-5xl xl:text-7xl 2xl:text-9xl'>{movie?.title || movie?.original_name}</h1>
-         <p className='max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl text-shadow-md'>
+         <h1 className='text-2xl font-bold 2xl:w-full md:text-4xl lg:text-5xl xl:text-7xl 2xl:text-9xl'>{movie?.title || movie?.original_name}</h1>
+         <p className='max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl text-shadow-xl'>
            {movie?.overview}
           </p>
 
