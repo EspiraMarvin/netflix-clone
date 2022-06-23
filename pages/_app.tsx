d7e254328b2285d7601a427366e8bce7ddfb45a7
marvin espira <espiramarvin@gmail.com>
2022-06-23 14:45:39 +0300

    adds HOC for recoil state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../hooks/useAuth'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // HOC- Higher Order Component
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    // HOC - RecoilRoot
+    <RecoilRoot>
+      {/* HOC/Higher Order Component - AuthProvider */}
+      <AuthProvider>  
+         <Component {...pageProps} />
+      </AuthProvider>
+    </RecoilRoot>
   )
 }
 
