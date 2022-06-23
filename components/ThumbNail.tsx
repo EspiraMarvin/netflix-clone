294bddbbf142b433c087a632ce31baae7ffe2613
marvin espira <espiramarvin@gmail.com>
2022-06-23 14:41:39 +0300

    updates

--- components/ThumbNail.tsx
+++ components/ThumbNail.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import { useRecoilState } from "recoil"
+import { modalState, movieState } from "../atoms/modalAtoms"
 import { Movie } from "../typings"
 
 interface Props {
@@ -9,8 +11,18 @@ interface Props {
 }
 
 function ThumbNail({ movie }: Props) {
+  const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+    <div 
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        // console.log('onclick movie', movie)
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
         <Image 
          src={`https://image.tmdb.org/t/p/w500${
             movie.backdrop_path || movie.poster_path
