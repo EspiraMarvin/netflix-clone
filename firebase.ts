5032ee4330372555c68b31e3fec8b94821f89444
marvin espira <espiramarvin@gmail.com>
2022-06-24 09:05:37 +0300

    firebase config change

--- firebase.ts
+++ firebase.ts
@@ -9,13 +9,12 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCiDnK3HSFDZ_e-QF2ZNTBVXZhng1cDNJY",
-    authDomain: "netflix-clone-b75e3.firebaseapp.com",
-    projectId: "netflix-clone-b75e3",
-    storageBucket: "netflix-clone-b75e3.appspot.com",
-    messagingSenderId: "401672775135",
-    appId: "1:401672775135:web:b8d5d60914d5096e2fd06a",
-    measurementId: "G-F8WYY6PP0Z"
+  apiKey: "AIzaSyDB6GYyY_gTMG3qH0tvbEFwpQc2X52wvk8",
+  authDomain: "netflix-clone-70812.firebaseapp.com",
+  projectId: "netflix-clone-70812",
+  storageBucket: "netflix-clone-70812.appspot.com",
+  messagingSenderId: "11673026506",
+  appId: "1:11673026506:web:c37d222e21e770930ad933"
 }
 
 // Initialize Firebase 
