858f7c8328e0de0ce762e121a0a8dbdae070623c
marvin espira <espiramarvin@gmail.com>
2022-06-24 17:08:42 +0300

    removed player component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react'
 import MuiModal from "@mui/material/Modal"
-import CircularProgress from '@mui/material/CircularProgress';
 import { useRecoilState } from 'recoil'
 import { modalState, movieState } from '../atoms/modalAtoms'
 import { Element, Genre } from '../typings'
-import ReactPlayer from 'react-player/lazy'
-import { FaPlay } from 'react-icons/fa'
-import { XIcon, PlusIcon, CheckIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon, PauseIcon } from '@heroicons/react/outline'
+import { XIcon } from '@heroicons/react/outline'
 import { format } from 'date-fns'
+import Player from './Player';
 
 interface trailerDetailsType {
   runtime: number | null
@@ -21,12 +19,7 @@ function Modal() {
     const [trailer, setTrailer] = useState('')
     const [trailerDetails, setTrailerDetails] = useState<trailerDetailsType | null>(null)
     const [genres, setGenres] = useState<Genre[]>([])
-    const [play, setPlay] = useState(false)
-    const [pause, setPause] = useState(false)
-    const [loading, setLoading] = useState(true)
-    const [muted, setMuted] = useState(false)
     const [showMore, setShowMore] = useState(true)
-    // const disableLoadingBtn = 
 
 
     useEffect(() => {
@@ -93,85 +86,14 @@ function Modal() {
         <>
          <button
             onClick={handleClose}
-            disabled={loading}
             className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818]
             hover:bg-[#181818]"
          >
             <XIcon className="w-6 h-6" />
          </button>
 
-         <div className="relative pt-[56.25%]">
-          <ReactPlayer
-              playing={play}
-              url={`https://www.youtube.com/watch?v=${trailer}`}
-              width="100%"
-              height="100%"
-              style={{ position: 'absolute', top: '0', left: '0' }}
-              muted={muted}
-              onReady={(() => setPlay(true))}
-              onBuffer={(() => {
-                setLoading(false)
-                setPause(true)
-              })
-            }
-            />
-
-         <div className="absolute flex items-center justify-between w-full px-10 bottom-10">
-           <div className="flex space-x-2">
-              <button
-                 onClick={() => {
-                  setPlay(!play)
-                  setPause(!pause)
-                }}
-                 className="flex items-center gap-x-2 rounded bg-white px-5 md:px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]"
-                 >
-                  {/* display play */}
-                  { !play && !pause && !loading &&
-                        (
-                          <>
-                            <FaPlay className="mt-1 text-black h-7 w-11" /> 
-                          Play
-                        </>
-                        )
-                  }
-                  
-                  {/* display pause */}
-                  { play && pause && !loading &&
-                    (
-                      <>  
-                        <PauseIcon className="mt-1 text-black h-7 w-7" />
-                        Pause
-                      </>
-                    ) 
-                  
-                  }
-                      
-                   {/* display loading  */}
-                  { loading &&
-                    (
-                    <div className='w-20'>
-                      <CircularProgress size={25} thickness={4} className="!text-black !mt-1" /> 
-                    </div>
-                    ) 
-                  }
-              </button>
-              <button className="modalButton">
-                <PlusIcon className="h-7 w-7" />
-              </button>
-              <button className="modalButton">
-                <ThumbUpIcon className="w-6 h-6" />
-              </button>
-           </div>
-           <button className="modalButton" onClick={() => setMuted(!muted)}>
-            { muted  ?
-              ( <VolumeOffIcon className="w-6 h-6" />)
-                :
-              (<VolumeUpIcon className="w-6 h-6" /> ) 
-            }
-           </button>
-         </div>
-         </div>
-
+         {/* movie player component */}
+           <Player trailer={trailer} />
 
          <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
             <div className="space-y-6 text-lg">
