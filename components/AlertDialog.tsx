3178ea10fc31288812a8ca465e4e73fc8367cb9c
marvin espira <espiramarvin@gmail.com>
2022-06-25 07:52:50 +0300

    alert dialog component

---
+++ components/AlertDialog.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+interface AlertDialogProp {
+  err: any
+}
+
+function AlertDialog({ err }: AlertDialogProp) {
+  const [open, setOpen] = useState(true)
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title text-red-800">
+        <div className='grid text-red-700 place-items-center'>Error!</div>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          { err || " Unexpected Error Occured. " }
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        {/* <Button onClick={handleClose}>Disagree</Button> */}
+        <Button onClick={handleClose} autoFocus>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </div>
+  )
+}
+
+export default AlertDialog
