3ce3afdc2f3d0c97953fc5d057f62ecaa85ef34d
marvin espira <espiramarvin@gmail.com>
2022-07-02 23:03:28 +0300

    transpiler added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withTM = require('next-transpile-modules')
+([
+  '@stripe/firestore-stripe-payments'
+]) // pass the modules you would like to see transpiled
+
+
+module.exports = withTM({
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org', 'rb.gy']
   }
-}
+})
