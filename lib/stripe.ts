3bd4e29334b1e72fa6333a8fb420abaea3b23e96
marvin espira <espiramarvin@gmail.com>
2022-07-02 23:04:41 +0300

    stripe updates

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -9,3 +9,15 @@ const payments = getStripePayments(app, {
     productsCollection: "products",
     customersCollection: "customers"
 })
+
+const loadCheckout =  async (priceId: string) => {
+    await createCheckoutSession(payments, {
+        price: priceId,
+        success_url: window.location.origin, // uses actual domain of your app
+        cancel_url: window.location.origin 
+    }).then(snapshot => window.location.assign(snapshot.url))
+    .catch(error => console.log(error.message))
+}
+
+export { loadCheckout }
+export default payments
