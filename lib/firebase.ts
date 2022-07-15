0ee161c52d61fa45f5cda5a8ae3d5238163a221a
marvin espira <espiramarvin@gmail.com>
2022-07-15 11:08:59 +0300

    adds analytics

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
+import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,11 +16,16 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase 
 // check if the app is not initialized, then initialize 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+if(typeof window != undefined){
+  getAnalytics(app)
+}
 const db = getFirestore()
 const auth = getAuth()
 export default app
