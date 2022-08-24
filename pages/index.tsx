72710063cc7f4206903dd6c365892d42752c3b1b
marvin espira <espiramarvin@gmail.com>
2022-08-24 23:10:18 +0300

    bix fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,7 +63,7 @@ const Home = ({
       <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
          <Banner netflixOriginals={netflixOriginals} />
         <section className="md:space-y-24">
-          <Row title="sfdkdf Now" movies={trendingNow} />
+          <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
 
